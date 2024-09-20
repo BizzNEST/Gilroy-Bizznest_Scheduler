bab1dd4004fe708b0be6129f1a5ad265d9bb4f67
Lisandro Jimenez <94939455+LJimenez88@users.noreply.github.com>
2024-09-20 16:19:25 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", function(){
     
     //function to click on export button and download
     document.getElementById("export-btn").addEventListener("click", function(){
-        //make sure to target the container you need
-        html2canvas(document.getElementById(/*add container name*/)).then(function(canvas){
+        //make sure to target the container you need down below
+        html2canvas(document.getElementById()).then(function(canvas){
             let imgData = canvas.toDataURL("image/png");
 
             let link = document.createElement('a');
             link.href = imgData;
-            link.download = /*title of exported image*/
+            link.download = "randomizerCapture"
 
             document.body.appendChild(link);
         link.click();
